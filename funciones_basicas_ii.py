abe379d31c83434ecc23d1477125e5b17f44cf47
Patricio Fuentealba <patricio.fuentealba.feliu@gmail.com>
2021-06-07 19:39:43 -0400

    fucniones basicas ii

---
+++ funciones_basicas_ii.py
@@ -0,0 +1,40 @@
+def countdown(p_desde):    
+    v_result=[]
+    for i in range(p_desde,-1,-1):
+        result.append(i)
+    return v_result    
+
+def PrintAndReturn(p_lista):
+    print(p_lista[0])
+    return(p_lista[1])
+
+def FirstPlusLen(p_lista):
+    return(p_lista[0]+len(p_lista))
+
+def gtSecond(p_lista):
+    v_result=[]
+    if len(p_lista)>3:
+        for v_valor in p_lista:
+            if(v_valor>p_lista[1]):
+                v_result.append(v_valor)
+        print(len(v_result))
+        return v_result
+    else:
+        return False
+
+def ThisLonThatVal(p_size,p_val):
+    v_result=[]
+    for v_indice in range(0,p_size):
+        v_result.append(p_val)
+    return v_result
+#print(countdown(2))
+
+#print(PrintAndReturn([1,2]))
+
+#print(FirstPlusLen([1,2,3,4,5]))
+
+#print(gtSecond([5,2,3,2,1,4]))
+
+#print(gtSecond([5]))
+
+#print(ThisLonThatVal(7,4))
--- index.py
+++ index.py
@@ -1,7 +1,39 @@
-saludo= 'Saludos desde un archivo Python'
-print(saludo)
 
-nombre='Patricio'
-edad=43
+#saludo= 'Saludos desde un archivo Python'
+#print(saludo)
 
-print('Mi nombre es '+ nombre+' y mi edad es '+str(edad))
+#nombre='Patricio'
+#edad=43
+
+#print('Mi nombre es '+ nombre+' y mi edad es '+str(edad))
+
+#def cubo(num):
+#    x=10
+#    x+=1
+#    print(x)
+#    return num*num*num
+
+#print(cubo(3))
+
+ciudades=['Iquique','Los Angeles','Chillán','Guayaquil','Lima']
+
+for ciudad in ciudades:
+    if ciudad[0]=='L':
+        continue
+    print(ciudad)
+
+i=1
+while i < 1000:
+    print(i)
+    i+=3
+
+while True:
+    nombre= input('Ingrese un nombre(o 0 para salir)')
+    if (nombre=="0"):
+        break
+    print('Hola ',nombre)
+
+def a():
+    print(5)
+x = a()
+print(x)
